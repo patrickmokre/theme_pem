a209e98e0469802b7ecd85e3a93a06c64a455bb6
patrickmokre <mokrp960@newschool.edu>
2019-04-24 14:17:55 +0200

    New BW theme, remove rm()

--- pemtheme-01.R
+++ pemtheme-01.R
@@ -1,5 +1,3 @@
-rm(list=ls())
-
 library(ggplot2)
 library(gridExtra)
 library(dplyr)
@@ -52,5 +50,51 @@ theme_pem <- function(base_size = 10,
                                       hjust=0.5),
       
       complete = FALSE
+    )
+}
+
+theme_pem_bw <- function(base_size = 10,
+                      base_family = "",
+                      base_line_size = base_size / 170,
+                      base_rect_size = base_size / 170){
+  theme_minimal(base_size = base_size, 
+                base_family = base_family,
+                base_line_size = base_line_size) %+replace%
+    theme(plot.title=element_text(size=15,
+                                  face="bold",
+                                  family="Montserrat",
+                                  color="#000000",
+                                  hjust=0),
+          plot.subtitle = element_text(size=10,
+                                       face="bold",
+                                       family="Montserrat",
+                                       hjust=0),
+          plot.caption = element_text(size=10,
+                                      face="plain",
+                                      hjust=1),
+          axis.title = element_text(size=8,
+                                    face="plain",
+                                    family="Esteban",
+                                    color="#000000"),
+          text = element_text(size=8,
+                              face="plain",
+                              family="Esteban",
+                              color="#000000",
+                              hjust = 0.5, 
+                              vjust = 0.5, 
+                              angle = 0, 
+                              lineheight = 0.9,
+                              margin = margin(), 
+                              debug = FALSE),
+          plot.background = element_rect(fill="#fcfcfc",
+                                         color="#fcfcfc"),
+          panel.grid = element_line(color="#bbbbbb",
+                                    size=0.5),
+          legend.background = element_rect(fill="#ffffff",
+                                           color="#fcfcfc"),
+          legend.title = element_text(face="bold",
+                                      hjust=0.5),
+          
+          complete = FALSE
     )
 }
